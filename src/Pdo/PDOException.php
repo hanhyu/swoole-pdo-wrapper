45c540758a20d08cd5ff0a84a2dd02941de9eda7
xiaohui.lam <xiaohui.lam@icloud.com>
2018-05-01 02:02:32 +0800

    init

---
+++ src/Pdo/PDOException.php
@@ -0,0 +1,13 @@
+<?php
+namespace Kuaiapp\Db\Pdo;
+
+/**
+ * Represents an error raised by PDO. You should not throw a <b>PDOException</b> from your own code.
+ * 
+ * @link http://php.net/manual/en/class.pdoexception.php
+ * @see  http://php.net/manual/en/language.exceptions.php Exceptions in PHP
+ */
+class PDOException extends RuntimeException
+{
+    public $errorInfo;
+}
